2b041103c42104acc3d1a7a2d5861f1f22bdb40c
wesleyara <wes1gom@gmail.com>
2024-05-18 14:10:17 -0300

    feat: adding tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -64,4 +64,53 @@ describe("lib tests", () => {
       expect(error.message).toBe("Internal server error");
     }
   });
+
+  it("get boards", async () => {
+    const boards = await simpleTrello.getBoards();
+
+    expect(boards).toBeInstanceOf(Array);
+  });
+
+  it("get lists", async () => {
+    const boardId = process.env.BOARD_ID as string;
+    const lists = await simpleTrello.getLists({ boardId });
+
+    expect(lists).toBeInstanceOf(Array);
+  });
+
+  it("get cards", async () => {
+    const listId = process.env.LIST_ID as string;
+    const cards = await simpleTrello.getCards({ listId });
+
+    expect(cards).toBeInstanceOf(Array);
+  });
+
+  it("get boards with error", async () => {
+    const simpleTrello = new SimpleTrello({
+      key: "invalid_key",
+      token: "invalid_token",
+    });
+
+    try {
+      await simpleTrello.getBoards();
+    } catch (error) {
+      expect(error.message).toBe("Internal server error");
+    }
+  });
+
+  it("get lists with error", async () => {
+    try {
+      await simpleTrello.getLists({ boardId: "invalid-board-id" });
+    } catch (error) {
+      expect(error.message).toBe("Internal server error");
+    }
+  });
+
+  it("get cards with error", async () => {
+    try {
+      await simpleTrello.getCards({ listId: "invalid-list-id" });
+    } catch (error) {
+      expect(error.message).toBe("Internal server error");
+    }
+  });
 });
